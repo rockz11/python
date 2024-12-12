08dcef9be821ee2ed5d71765ba497763b65b455b
Prughvi <prudhviraj3525@gmail.com>
2024-12-12 15:28:55 +0530

    m

---
+++ program.py
@@ -0,0 +1,18 @@
+print("Hello INDIA")
+print(type(2))
+a =11
+print(id(a))
+a = 2
+a*= 2
+print(a)
+name = input("Please enter your name: ")
+print(type(name))
+print("Hello," + name)
+a = [1,2,3,4,5, "cloud",'true']
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[5])
+print(a[6])
+print(len(a))
+print("length of the list:", 7)

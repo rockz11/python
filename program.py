4fc8704ce164aa8e801a43b67c1f1ff9b16d61e4
Prughvi <prudhviraj3525@gmail.com>
2024-12-12 17:51:55 +0530

    m

--- program.py
+++ program.py
@@ -1,19 +1,21 @@
-print("Hello INDIA")
-print(type(2))
-a =11
-print(id(a))
-a = 2
-a*= 2
-print(a)
-name = input("Please enter your name: ")
-print(type(name))
-print("Hello," + name)
-a = [1,2,3,4,5, "cloud",'true']
-print(a[0])
-print(a[1])
-print(a[2])
-print(a[5])
-print(a[6])
-print(len(a))
-print("length of the list:", 7)
-print(a,len(a))
+# print("Hello INDIA")
+# print(type(2))
+# a =11
+# print(id(a))
+# a = 2
+# a*= 2
+# print(a)
+# name = input("Please enter your name: ")
+# print(type(name))
+# print("Hello," + name)
+# a = [1,2,3,4,5, "cloud",'true']
+# print(a[0])
+# print(a[1])
+# print(a[2])
+# print(a[5])
+# print(a[6])
+# print(len(a))
+# print("length of the list:", 7)
+# print(a,len(a))
+a = [1,2,3,4,5,6,7,8,9] # slicing operation on list
+print(a[0:8])

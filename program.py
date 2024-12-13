b4bdcd9862567ea6afd9637975ac206915202d35
Prughvi <prudhviraj3525@gmail.com>
2024-12-13 12:00:47 +0530

    m

--- program.py
+++ program.py
@@ -22,9 +22,9 @@
 # print(a[len(a) -1])
 # print(len(a))
 # # print(a[::2])
-# a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-# print(a[-1::-1])
-# print(a[::-1])
+a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+print(a[-1::-1])
+print(a[::-1])
 
 #Appending and Extending
 # This means appending values to a list 
